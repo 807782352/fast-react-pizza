fc9af45e4bc9fa6136756bf7c1c1c9f00d49a56d
z297xu <kyrie807782352@gmail.com>
2023-11-29 16:50:28 -0500

    setup file structure and simple router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-export default function App() {
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-  const x = 0; 
-  
-  return (
-    <div>
-      Hello Vite !!
-    </div>
-  )
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "../starter/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "../starter/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
